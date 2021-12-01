5b834b6683d75adeec8cfa1fd89efbc64d13681c
bettyvng <betty.vg8@gmail.com>
2021-12-01 21:38:42 +0000

    pair programming activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word) || l.includes(word.split("").reverse().join(""))) {
+            return true;
+        }
+        let verticalWord = transpose(letters);
+        let verticalJoin = verticalWord.map((ls) => ls.join(""));
+        for (let l of verticalJoin) {
+            if (l.includes(word) || l.includes(word.split("").reverse().join("")))  {
+                return true;
+            }
+        }
     }
-}
+    return false;
+};
+
+// matrix code
+
+const transpose = function(matrix) {
+    let newArray = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      newArray.push([]);
+    }
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        newArray[j][i] = matrix[i][j];
+      }
+    }
+  
+    return newArray;
+  };
 
 module.exports = wordSearch
